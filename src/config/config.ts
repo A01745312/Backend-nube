7c2eee807bab29580154ebd7711b731f0b8b4160
Paula Santoyo <a01745312@itesm.mx>
2023-05-29 18:31:36 -0600

    Carpeta config

---
+++ src/config/config.ts
@@ -0,0 +1,26 @@
+import { DB_NAME,DB_USER,DB_PASSWORD,DB_HOST } from ".";
+
+
+export default{
+  "development": {
+    "username": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+export const PORT:number = process.env.PORT ? +process.env.PORT:8080;
+export const NODE_ENV:string = process.env.NODE_ENV ? process.env.NODE_ENV:'development';
+export const DB_NAME:string = process.env.DB_NAME ? process.env.DB_NAME:'prueba'; 
+export const DB_USER:string = process.env.DB_USER ? process.env.DB_USER:'root'; 
+export const DB_PASSWORD:string = process.env.DB_PASSWORD ? process.env.DB_PASSWORD:'password'; 
+export const DB_HOST :string = process.env.DB_HOST ? process.env.DB_HOST:'localhost'; 
+export const AWS_REGION:string = process.env.AWS_REGION ? process.env.AWS_REGION:''; 
+export const AWS_ACCESS_KEY:string = process.env.AWS_ACCESS_KEY ? process.env.AWS_ACCESS_KEY:'';
+export const AWS_SECRET_ACCESS_KEY:string = process.env.AWS_SECRET_ACCESS_KEY ? process.env.AWS_SECRET_ACCESS_KEY:'';
+export const AWS_SESSION_TOKEN:string = process.env.AWS_SESSION_TOKEN? process.env.AWS_SESSION_TOKEN:'';
+export const PREFIX_TABLE = NODE_ENV === "production" ? '':'-DEV';
+export const COGNITO_APP_CLIENT_ID = process.env.COGNITO_APP_CLIENT_ID?process.env.COGNITO_APP_CLIENT_ID:'';
+export const COGNITO_APP_SECRET_HASH = process.env.COGNITO_APP_SECRET_HASH?process.env.COGNITO_APP_SECRET_HASH:'';
+export const COGNITO_USER_POOL_ID = process.env.COGNITO_USER_POOL_ID?process.env.COGNITO_USER_POOL_ID:'';
